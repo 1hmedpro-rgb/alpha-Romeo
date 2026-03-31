59ee0cb4383d5737a561e156b3ced062a7fb75f7
1hmedpro-rgb <1hmed.pro@gmail.com>
2026-03-31 19:00:45 +0200

    ksdljfhshkfdsjkh

--- php/traitementFormulaire.php
+++ php/traitementFormulaire.php
@@ -43,7 +43,7 @@ echo "<a href='../html/Ecole.html'>Retour à l'accueil</a>";
 
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
