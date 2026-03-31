d6d4832490bd3c9fd1299d008357bff362a6e4d9
1hmedpro-rgb <1hmed.pro@gmail.com>
2026-03-31 17:33:51 +0200

    dofhjijdfh

--- php/mail.php
+++ php/mail.php
@@ -12,13 +12,69 @@ $contenu = "Nom: $nom\nEmail: $email\nMessage:\n$message";
 $entetes = "From: " . $email;
 
 // 3. On envoie l'email
-if (mail($destinataire, $sujet, $contenu, $entetes)) {
-        // Si ça marche, on affiche un message
-        echo "<h1>Super ! Votre message a bien été envoyé.</h1>";
-        echo "<a href='../html/Ecole.html'>Retour à l'accueil</a>";
-    } else {
-        // S'il y a une erreur technique
-        echo "<h1>Oups, une erreur est survenue lors de l'envoi.</h1>";
-    }
-
+(mail($destinataire, $sujet, $contenu, $entetes))
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Statut de l'envoi</title>
+    <link rel="stylesheet" href="../css/mail.css">
+</head>
+<body>
+
+    <div id="haut-de-page">
+        <header>
+            <nav> 
+                <a href ="../html/Ecole.html"> <img src="../image/logo_efrei.png" alt="logo_efrei" id="logo"></a>
+                <a href="../html/Ecole.html" class="lien">L'ECOLE</a>
+                <a href="../html/cours_et_formations.html" class="lien">COURS / FORMATIONS</a>
+                <a href="../html/equipe_enseignante.html" class="lien">EQUIPE ENSEIGNANTE</a>
+                <a href="../html/contactez_nous.html" class="lien">CONTACTEZ-NOUS</a>
+                <a href="../html/a_propos.html" class="lien">A PROPOS</a>
+            </nav>
+        </header>
+    </div>
+
+    <?php if ($envoi_reussi): ?>
+        <div class="boite-message">
+            <h1>Super !</h1>
+            <p>Merci <strong><?php echo $nom; ?></strong>. Votre message a bien été envoyé, nous vous répondrons très vite à l'adresse <strong><?php echo $email; ?></strong>.</p>
+            <a href="../html/Ecole.html" class="btn-retour">Retour à l'accueil</a>
+        </div>
+    <?php else: ?>
+        <div class="boite-message erreur">
+            <h1>Oups !</h1>
+            <p>Désolé <?php echo $nom; ?>, une erreur technique est survenue. Votre message n'a pas pu être envoyé.</p>
+            <a href="../html/Ecole.html" class="btn-retour">Retour à l'accueil</a>
+        </div>
+    <?php endif; ?>
+
+
+    <footer>    
+        <div class ="divs">
+            <h3 id="div1">NAVIGATION</h3><br><br>
+            <a href="../html/Ecole.html" class="lien_footer">L'ECOLE</a><br><br>
+            <a href="../html/cours_et_formations.html" class="lien_footer">COURS / FORMATIONS</a>
+        </div>
+            
+        <div class ="divs">
+            <h3 id="div2">LIENS RAPIDES</h3><br><br>
+            <a href="../html/equipe_enseignante.html" class="lien_footer">EQUIPE ENSEIGNANTE</a><br><br>
+            <a href="../html/a_propos.html" class="lien_footer">A PROPOS</a>
+        </div>
+            
+        <div class ="divs"> 
+            <h3 id="div3">CONTACT</h3><br><br>
+            <a href="../html/contactez_nous.html" class="lien_footer">CONTACTEZ-NOUS</a>
+        </div>
+    </footer>
+
+
+
+</body>
+</html>
